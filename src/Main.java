6ba687b67d46a905b4093932eb474b35b7ccedae
future88075620 <future870914@gmail.com>
2017-09-25 15:57:04 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a,b,c,d,e;
+        int x,y;
+        Scanner input=new Scanner(System.in);
+        x= input.nextInt();
+        y= input.nextInt();
+        System.out.printf("%d+%d=%d\n",x,y,x+y) ;
+        System.out.printf("%d-%d=%d\n",x,y,x-y) ;
+        System.out.printf("%d*%d=%d\n",x,y,x*y) ;
+        System.out.printf("%d/%d=%d...%d\n",x,y,x/y,x%y) ;
+        input.close();
+    }
+}
